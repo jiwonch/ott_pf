2837e805271e867f49eecdb085c74c8f5cc2161f
JIWON CHOI <rizjw26@gmail.com>
2022-05-12 12:53:59 +0900

    20220512_코드 정리

--- js/login.js
+++ js/login.js
@@ -1,22 +1,22 @@
-window.onload=function(){
-            var modal = document.getElementById("main_login");
+window.onload = function () {
+    var modal = document.getElementById("main_login");
 
-        var btn = document.getElementById("myBtn");
+    var btn = document.getElementById("myBtn");
 
-        var span = document.getElementsByClassName("close")[0];
+    var span = document.getElementsByClassName("close")[0];
 
-        btn.onclick=function(){
-            modal.style.display="block"
+    btn.onclick = function () {
+        modal.style.display = "block"
+    }
+
+    span.onclick = function () {
+        modal.style.display = "none"
+    }
+
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none"
         }
-
-        span.onclick=function(){
-            modal.style.display="none"
-        }
-
-        window.onclick =function(event){
-            if(event.target==modal){
-                modal.style.display="none"
-            }
-        }
+    }
 
 }
